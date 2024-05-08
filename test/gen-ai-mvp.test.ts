d7202d82ae6e50a33fa66d0351ed82e7e18d68e1
Rudra Sett <rudro.sett@gmail.com>
2024-05-08 14:38:50 -0400

    first commit

---
+++ test/gen-ai-mvp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GenAiMvp from '../lib/gen-ai-mvp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/gen-ai-mvp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GenAiMvp.GenAiMvpStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
