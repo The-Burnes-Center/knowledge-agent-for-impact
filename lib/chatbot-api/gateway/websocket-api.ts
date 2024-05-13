0a5901c9a222d987117c69a91d82cd9cddddeec8
Rudra Sett <rudro.sett@gmail.com>
2024-05-13 15:30:47 -0400

    changed name back to prod

--- lib/chatbot-api/gateway/websocket-api.ts
+++ lib/chatbot-api/gateway/websocket-api.ts
@@ -20,7 +20,7 @@ export class WebsocketBackendAPI extends Construct {
     super(scope, id);
     // Create the main Message Topic acting as a message bus
     const webSocketApi = new apigwv2.WebSocketApi(this, 'wsAPI');
-    const webSocketApiStage =  new apigwv2.WebSocketStage(this, 'wsAPI-prod-stage', {
+    const webSocketApiStage =  new apigwv2.WebSocketStage(this, 'wsAPI-prod', {
       webSocketApi,
       stageName: 'prod',
       autoDeploy: true,      
