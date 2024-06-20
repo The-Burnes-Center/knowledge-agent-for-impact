ec85c12cb811ded943983394aa77bd8636ffa496
Rudra Sett <rudro.sett@gmail.com>
2024-06-20 10:43:00 -0400

    set OIDC provider as blank by default

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,6 +7,7 @@ export const kendraIndexName = 'gen-ai-chatbot-index'
 export const cognitoDomainName = "genaimvp-auth"
 // this can be anything that would be understood easily, but you must use the same name
 // when setting up a sign-in provider in Cognito
-export const OIDCIntegrationName = "Azure-OIDC"
+// make sure to leave it blank if you do not actually have an SSO provider configured in Cognito!
+export const OIDCIntegrationName = ""
 // this MUST be unique to your account
 export const stackName = "GenAiChatStack"
